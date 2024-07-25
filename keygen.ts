a86945d2188e18378ea2f1b672435455970d16cc
RutamBhagat <rutambhagat@gmail.com>
2024-07-25 16:45:48 +0530

    save generated key to dev-wallet.json

--- keygen.ts
+++ keygen.ts
@@ -1,6 +1,11 @@
 import { Keypair } from "@solana/web3.js";
+import fs from "fs";
 
 //Generate a new keypair
 let kp = Keypair.generate();
 console.log(`You've generated a new Solana wallet: ${kp.publicKey.toBase58()}`);
-console.log("kp.secretKey", kp.secretKey);
+const secretKey = Array.from(kp.secretKey);
+console.log("secretKey", secretKey);
+
+// write the keypair.secretKey to dev-wallet.json
+fs.writeFileSync("dev-wallet.json", JSON.stringify(secretKey));
